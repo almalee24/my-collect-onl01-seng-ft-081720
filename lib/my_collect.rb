b38f76114570d936fb51af38e281e5a4b4fac021
Alma Flores <flores.almalee@gmail.com>
2020-08-18 23:59:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
-def my_collect(collection)
-  i = 0 
-  while i < collection.length 
-    yield(collection[i])
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length 
+    collection << yield(array[i])
     i += 1 
   end
   collection
